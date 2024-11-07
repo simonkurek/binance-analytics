ec5bd71317079ab55f1d5f62031394b0561084d1
Szymon Kurek <kurek@devanddeliver.com>
2024-11-07 14:15:13 +0100

    fix(Mongo): remove deprecated options

--- src/common/config/mongo.config.ts
+++ src/common/config/mongo.config.ts
@@ -2,8 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('mongodb', () => ({
   uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/binance',
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
 }));
