d509c33dff182b061d2725fd2ed7039c1bce8c4b
Szymon Kurek <kurek@devanddeliver.com>
2024-11-07 12:51:04 +0100

    feat(mongo): init mongo module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.getOrThrow('mongodb');
+        return {
+          uri: config.uri,
+          ...config.options,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class CommonModule {}
